5b4ab278d74cca48509f3d5df00a0251cf0541f6
Muhammad Javed <theahmadzai@hotmail.com>
2026-01-15 21:27:00 -0400

    Update rention seconds for busy times

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const ORDERS_RETENTION_SECONDS = 604800; // 7 days in seconds
-export const BUSY_TIMES_RETENTION_SECONDS = 900; // 15 minutes in seconds
+export const BUSY_TIMES_RETENTION_SECONDS = 86400; // 1 day in seconds
